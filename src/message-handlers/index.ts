895ed1ba6b175ef1d78f8fcd600f9d72b7b42b36
Shilo Magen <smangam@gmail.com>
2022-06-14 21:12:02 +0300

    Add notification queue to whatsapp webhook

--- src/message-handlers/index.ts
+++ src/message-handlers/index.ts
@@ -4,19 +4,12 @@ import { MessageTypes } from '../utils/cloud-api-payload-extractor/cloud-api-pay
 import { LoggerMessages } from '../services/logger';
 import { Message } from '../external-types/messages';
 import { handleAudio } from './audio';
-import Content from '../content.json';
 
 export type MessageHandler<T extends MessageBody = any> = (message: T, phoneNumberId: string, services: Services) => Promise<void>;
 
 const genericHandler: MessageHandler<Message> = async (message, phoneNumberId, services) => {
-  const { logger, publisher } = services;
+  const { logger } = services;
   logger.info({ message }, LoggerMessages.ReceivedUnsupportedMessage);
-  await publisher.publishToNotificationQueue({
-    fromId: phoneNumberId,
-    to: message.from,
-    text: Content.IncomingMessageNotSupported
-  });
-
 };
 
 export const MessageTypeToHandler: Record<MessageTypes, MessageHandler> = {
